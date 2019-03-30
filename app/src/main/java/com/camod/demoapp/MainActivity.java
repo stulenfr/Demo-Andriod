2832cf90abba21d7a4edea02f5f1531c5b753047
Foster Stule <camodude5@yahoo.com>
2019-03-29 22:21:05 -0400

    made a toast to pop up with the textfeild value

--- app/src/main/java/com/camod/demoapp/MainActivity.java
+++ app/src/main/java/com/camod/demoapp/MainActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view){
-
         EditText myTextFeild = (EditText) findViewById(R.id.myTextField);
+
+        Toast.makeText(MainActivity.this, myTextFeild.getText().toString()+"!", Toast.LENGTH_LONG).show();
 
         Log.i("Info", myTextFeild.getText().toString());
     }
